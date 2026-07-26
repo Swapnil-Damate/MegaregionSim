fb66dbb8c7881ba0de7df10c7038c3b16ea0b406
Swapnil Anil Damate <swapnilanildamate@Swapnils-MacBook-Air.local>
2026-07-27 01:52:03 +0530

    Fixed missing Coupler declarations in TrainPawn header

--- Source/MegaregionSim/TrainPawn.h
+++ Source/MegaregionSim/TrainPawn.h
@@ -92,4 +92,17 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Train Input")
 	void SetTargetBrakePressure(float TargetPressure);
+
+	// Physical Constraints acting as Knuckle Couplers
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coupler")
+	UPhysicsConstraintComponent* RearCoupler;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coupler")
+	USphereComponent* RearCouplerTrigger;
+
+	UPROPERTY()
+	AActor* RearAttachedCar;
+
+	UFUNCTION()
+	void OnCouplerOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
