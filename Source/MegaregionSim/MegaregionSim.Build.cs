b96094bdc806f37ffdd2660cd79973093518799e
Swapnil Anil Damate <swapnilanildamate@Swapnils-MacBook-Air.local>
2026-07-27 22:57:27 +0530

    Fix compile error: Add AIModule to Build.cs

--- Source/MegaregionSim/MegaregionSim.Build.cs
+++ Source/MegaregionSim/MegaregionSim.Build.cs
@@ -8,7 +8,7 @@ public class MegaregionSim : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore", "UMG", "WebBrowserWidget", "WebBrowser", "SQLiteCore", "SQLiteSupport", "PCG", "Landscape", "Foliage" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore", "UMG", "WebBrowserWidget", "WebBrowser", "SQLiteCore", "SQLiteSupport", "PCG", "Landscape", "Foliage", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
