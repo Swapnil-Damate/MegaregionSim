ec953a37cc34928f66630af1765532989c528c6b
Swapnil Anil Damate <swapnilanildamate@Swapnils-MacBook-Air.local>
2026-07-27 22:06:25 +0530

    Update BuildStaticMap to bake massive 40km procedural world

--- BuildStaticMap.py
+++ BuildStaticMap.py
@@ -19,33 +19,21 @@ def build_static_map():
     # 3. Spawn Train Blueprint on the track
     train_class = unreal.load_class(None, '/Game/Blueprints/BP_TrainPawn.BP_TrainPawn_C')
     if train_class:
-        train_actor = unreal.EditorLevelLibrary.spawn_actor_from_class(train_class, unreal.Vector(0, 0, 150))
+        # Spawned far out into the procedural world to see the new hills!
+        train_actor = unreal.EditorLevelLibrary.spawn_actor_from_class(train_class, unreal.Vector(0, -10000, 300))
         unreal.log("Spawned BP_TrainPawn on the tracks.")
 
-    # 4. Spawn some Diorama Scenery so you can visualize the assets!
-    # Load Skyscraper and Pine Tree meshes
-    skyscraper_mesh = unreal.EditorAssetLibrary.load_asset('/Game/FinalAssets/Skyscraper')
-    pine_tree_mesh = unreal.EditorAssetLibrary.load_asset('/Game/FinalAssets/Pine_Tree')
-    
-    # Spawn Static Mesh Actors
-    sma_class = unreal.load_class(None, '/Script/Engine.StaticMeshActor')
-    
-    if skyscraper_mesh:
-        # Spawn a small city cluster to the right of the tracks
-        for i in range(3):
-            city_bldg = unreal.EditorLevelLibrary.spawn_actor_from_class(sma_class, unreal.Vector(5000 + (i*2000), 4000, 0))
-            if city_bldg:
-                city_bldg.get_component_by_class(unreal.StaticMeshComponent).set_static_mesh(skyscraper_mesh)
-                
-    if pine_tree_mesh:
-        # Spawn a small forest to the left of the tracks
-        for i in range(10):
-            tree = unreal.EditorLevelLibrary.spawn_actor_from_class(sma_class, unreal.Vector((i*1000), -3000, 0))
-            if tree:
-                tree.get_component_by_class(unreal.StaticMeshComponent).set_static_mesh(pine_tree_mesh)
-                
+    # 4. Bake the Massive 40-Kilometer Procedural World!
+    master_controller_class = unreal.load_class(None, '/Script/MegaregionSim.MasterSimulationController')
+    if master_controller_class:
+        master_controller = unreal.EditorLevelLibrary.spawn_actor_from_class(master_controller_class, unreal.Vector(0, 0, 0))
+        if master_controller:
+            # Trigger the C++ procedural generation to bake thousands of trees and skyscrapers instantly!
+            master_controller.generate_environment()
+            unreal.log_warning("SUCCESS: 40-Kilometer Procedural World with Rolling Hills Generated!")
+
     unreal.log("--- STATIC MAP GENERATION COMPLETE ---")
-    unreal.log_warning("INSTRUCTIONS: Fly around the editor viewport to look at the Train, Tracks, City, and Forest!")
+    unreal.log_warning("INSTRUCTIONS: Fly around the editor viewport! The Train is at (0, -10000, 300) surrounded by the massive world!")
     unreal.log_warning("You can save this map by going to File -> Save Current Level As -> SimulatedMap")
 
 build_static_map()
