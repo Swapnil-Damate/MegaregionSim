543761b060575065a863be84583e11b5d2e92d97
Swapnil Anil Damate <swapnilanildamate@Swapnils-MacBook-Air.local>
2026-07-27 10:22:27 +0530

    Automated player train spawning so Blueprints are completely unnecessary

--- Source/MegaregionSim/AutomatedTrainSpawner.cpp
+++ Source/MegaregionSim/AutomatedTrainSpawner.cpp
@@ -2,10 +2,17 @@
 #include "MassEntitySubsystem.h"
 #include "TrainFragment.h"
 #include "Engine/World.h"
+#include "TrainPawn.h"
 
 void UAutomatedTrainSpawner::OnWorldBeginPlay(UWorld& InWorld)
 {
 	Super::OnWorldBeginPlay(InWorld);
+
+	// Zero-Manual-Entry: Automatically spawn the Player's Train so you don't need Blueprints!
+	FVector SpawnLocation(0.0f, 0.0f, 200.0f);
+	FRotator SpawnRotation(0.0f, 0.0f, 0.0f);
+	InWorld.SpawnActor<ATrainPawn>(ATrainPawn::StaticClass(), SpawnLocation, SpawnRotation);
+
 	SpawnVirtualTrains(500); // 500 AI trains mathematically simulated in the background!
 }
 
